1058c5e2e7fe484195baad9a3eb532ea4f978db5
doug y'barbo <doug.ybarbo@gmail.com>
2015-02-12 12:54:28 -0800

    added groupby transformation

--- etl-spark-01.scala
+++ etl-spark-01.scala
@@ -8,12 +8,31 @@
 // %> ./bin/spark-shell --master local[*]
 
 
+// one line of the data:
+String = 37951,70084,1,?,0.125,?,1,0,0,1,0,FALSE
+
+
 val data_dir = "/Users/doug/scala_projects/aa_spark/ch02-intro/linkage"
 
 val rawblocks = sc.textFile(data_dir) 
 
-val hx = rawblocks.take(10)
+// get # lines in rawblocks
+rawblocks.count()
 
+// 6,552,407
+
+// hx is just a tiny subset of the complete dataset 'head'
+// an action returns data, so this returns Array[String]
+val hx = rawblocks.take(100)
+
+// get 5000 lines of data without replacement)val 
+val hx = rawblocks.takeSample(false, 5000)
+
+// note: take * takeSample are 'actions';
+// 'sample' is a 'transformation'
+
+// get one row of data from hx
+val line = hx(4)
 
 // remove headers from data (the RDD bound to "hx")
 
@@ -62,11 +81,13 @@ t.productArity			// get tuple size
 
 
 // create a human-readable record tuple, using scala case classes
+// one argument for each element in the tuple
 
 case class MatchData(id1: Int, id2: Int,
 	s: Array[Double], m: Boolean)
 
 // udpate 'parse' fn so it returns instance of MatchData case class
+// instead of a tuple
 
 def parse(line: String) = {
 	val p = line.split(',')
@@ -77,18 +98,43 @@ def parse(line: String) = {
 	MatchData(id1, id2, s, m)
 }
 
-
+// random grab one line from the mini-dataset 'hx'
+val line = hx(100)
 val md = parse(line)
-
 
 // apply the parse fn to all elements in hx except header line:
 
 val mds = hx.filter(x => !isHeader(x)).map(x => parse(x))
-
 
 // apply parse to all data in the cluster:
 
 val data1 = data.map(line => parse(line))
 
 data1.cache()
+// store the RDD the next time it's calculated
+// 'cache()' is syntactic sugar for rdd.persist(StorageLevel.MEMORY)
+// which stores the RDD as unserialized java objects
+// t/4 no serialization overhead
+// alternatie mechanism in spark for persisting RDDs 'caching':
+// rdd.persist(StorageLevel.MEMORY_SER), allocates large byte buffers
+// in RAM & serializes the RDD contents into them 
+// disk-based storage levels: MEMORY_AND_DISK, MEMORY_AND_DISK_SER
+// http://spark.apache.org/docs/latest/programming-guide.html#rdd-operations
+
+
+//---------------- aggregations -----------------//
+
+val g1 = mds.groupBy(md => md.m)
+// group the 10 lines of data based on the value in the 'm' field (T/F)
+
+
+val counts = g1.mapValues(x => x.size)
+// returns:
+// scala.collection.immutable.Map[Boolean,Int] = Map(false -> 2906, true -> 2093)
+
+counts.foreach(println)
+// get counts by calling mapValues on g1, which operates only on the values
+// returns: 
+// (false,2906)
+// (true,2093)
 
