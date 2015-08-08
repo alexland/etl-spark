33d28b7d756d86acb59680fbc5a38fc894d2a3ec
alexland <doug.ybarbo@gmail.com>
2015-08-07 18:09:47 -0700

    main build script and init script

---
+++ ALSRecommender/spark-app-init.sh
@@ -0,0 +1,28 @@
+#!/usr/local/bin/zsh
+
+# starter.sh
+
+##########
+#	compiles scala code written against the scala-spark api
+#	to a jar, then sends it to the spark cluster via the
+#	spark submitter
+#
+#	does not build spark, but relies on an extant install
+#	which is accessed by the environment variable below
+#
+#	environment variable SPARK_HOME must be set & alias
+#	top-level directory of spark src, eg set in .bash_profile, .zshrc, or other:
+#	export SPARK_HOME="$HOME/spark"
+#
+#
+##########
+
+sbt clean assembly
+
+$SPARK_HOME/bin/spark-submit \
+	--driver-memory 6g \
+	--class "SparkGrep" \
+	--master "local[*]" \
+	--deploy-mode client \
+	target/scala-2.10/SparkGrep.jar
+
